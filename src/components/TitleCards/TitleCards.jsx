dd3009e4151ddff44a8ed3bcffbe528ffee9086c
Chris <chrislanzas89@outlook.com>
2025-08-09 16:37:01 -0400

    Login Page and fetched movies from movie API database

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
@@ -6,7 +6,20 @@ import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
 
+const [apiData, setApiData] = useState([])
 const cardsRef = useRef();
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODEzN2ViOTUwNDFjZTM3MWI4ZDU3YTFmY2I3NTQ1MCIsIm5iZiI6MTc1NDc2OTUxOC4xNzEsInN1YiI6IjY4OTdhODZlODBmNWQwZWZjZWI2Zjk4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yVnxr1_HRclZZ_OFrK-4FOLkO8iD6EC9KS6RF9ZKZ7s'
+  }
+};
+
+
+
 
 const handleWheel = (event)=>{
 event.preventDefault();
@@ -14,6 +27,12 @@ cardsRef.current.scrollLeft += event.deltaY;
 }
 
 useEffect(()=> {
+
+fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel', handleWheel )
 }, [])
 
@@ -21,10 +40,10 @@ useEffect(()=> {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netlfix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
